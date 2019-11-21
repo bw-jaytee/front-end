8c63d164d45afd0ee9c8905ae19e41f576fae2f5
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-11-21 08:32:50 -0800

    PrivateRoute Redirect correction

--- gigapet-react-app/src/components/UserHome.js
+++ gigapet-react-app/src/components/UserHome.js
@@ -1,21 +1,17 @@
 import React, { useEffect } from "react";
 import { withRouter } from "react-router-dom";
-import { Button } from 'reactstrap';
-import { connect } from 'react-redux';
+import { Button } from "reactstrap";
+import { connect } from "react-redux";
 import FoodEntry from "./FoodEntry";
 import PetCard from "./PetCard";
 import "../styles.css";
-import { fetchUserData } from '../actions';
-
+import { fetchUserData } from "../actions";
 
 const UserHome = props => {
   console.log(props);
 
-  //this state will hold ALL user data: userId, name/info, food entry history, pet status as per backend set-up
-  // const [userData, setUserData] = useState({});
-
   useEffect(() => {
-   props.fetchUserData();
+    props.fetchUserData();
   }, []);
 
   const logOut = () => {
@@ -27,22 +23,24 @@ const UserHome = props => {
     props.history.push("/summary");
   };
 
+  //capitalizes first letters of fullname for welcome
   const formatName = name => {
-    var array1 = name.split(' ');
-  var newarray1 = [];
-    
-  for(var x = 0; x < array1.length; x++){
-      newarray1.push(array1[x].charAt(0).toUpperCase()+array1[x].slice(1));
-  }
-  return newarray1.join(' ');
+    var array1 = name.split(" ");
+    var newarray1 = [];
 
-  }
+    for (var x = 0; x < array1.length; x++) {
+      newarray1.push(array1[x].charAt(0).toUpperCase() + array1[x].slice(1));
+    }
+    return newarray1.join(" ");
+  };
 
   return (
     <>
       <header className="header">
         <h3>{`Welcome ${formatName(props.APIdata.fullname)}`}</h3>
-        <button className ="submitButton" onClick={logOut}>Log Out</button>
+        <button className="submitButton" onClick={logOut}>
+          Log Out
+        </button>
       </header>
 
       <div className="components">
